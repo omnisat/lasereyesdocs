cbccf7658f435099b103a1ecd560b6269d88bf3e
0xBunzy <bunzybuilds@gmail.com>
2024-07-21 18:37:22 -0400

    updates

--- docs/snippets/ConnectWallet.tsx
+++ docs/snippets/ConnectWallet.tsx
@@ -1,24 +1,12 @@
 // @/components/ConnectWallet.tsx
 function ConnectWallet() {
-    const { connect, disconnect } = useLaserEyes()
+  const { connect, disconnect } = useLaserEyes();
 
-    return (
-        <>
-            <button onClick={() => connect("oyl")}>
-                Connect Oyl Wallet
-            </button>
+  return (
+    <>
+      <button onClick={() => connect("unisat")}>Connect Unisat Wallet</button>
 
-            <button onClick={() => connect("unisat")}>
-                Connect Unisat Wallet
-            </button>
-
-            <button onClick={() => connect("xverse")}>
-                Connect XVerse Wallet
-            </button>
-
-            <button onClick={disconnect}>
-                Disconnect Wallet
-            </button>
-        </>
-    )
+      <button onClick={disconnect}>Disconnect Wallet</button>
+    </>
+  );
 }
